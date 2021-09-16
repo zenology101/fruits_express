439e9e2a9c7423689cceae9690f9d209638a3cd7
Zen Ahmed <zenahmed994@gmail.com>
2021-09-15 20:02:20 -0400

    added a post route and coded more in the views folder

--- sever.js
+++ sever.js
@@ -1,3 +1,15 @@
+/////////////////////////////////////////////////
+//order of the routes 
+// I - Index
+// N - New
+// D - Destroy
+// U - Update
+// C - Create
+// E - Edit
+// S - Show
+////////////////////////////////////////////////
+
+
 //we import the express library 
 const express = require('express');
 
@@ -7,14 +19,47 @@ const app = express();
 //imports my fruits data 
 const fruits = require("./models/fruits")
 
-//routes
+////////////////////////////////////////////
+//Middle Ware
+/////////////////////////////////
+//Parse Request Bodies if Content-Type Header is: 
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({extend: false})),
+
+/////////////////////////////////////////////
+//Routes 
+/////////////////////////////////////////////
+//Index route- gets all fruit 
 app.get('/fruits/', (req,res) =>{
     res.render("index.ejs", {allFruits: fruits});
 });
 
+// NEW ROUTE - Renders form to Create Fruit
+app.get("/fruits/new", (req, res) => {
+    res.render("new.ejs")
+  })
+
+//Create route using Post request 
+// Create Route - makes a new fruit
+app.post("/fruits", (req, res) => {
+    //process the readyToEat as true or false
+    if(req.body.readyToEat === 'on'){
+      req.body.readyToEat = true
+    } else {
+      req.body.readyToEat = false
+    }
+    // push req.body into the array of fruits
+    fruits.push(req.body)
+    //redirect (get) to /fruits
+    res.redirect("/fruits")
+  });
+
+  //show route- gets one fruit (keep your show route at the end)
+  //
 app.get('/fruits/:indexOfArrayFruits', (req,res) =>{
     res.render("show.ejs", {fruit: fruits[req.params.indexOfArrayFruits]});
 });
+
 
 //the listener sever 
 app.listen(3000, ()=>{
